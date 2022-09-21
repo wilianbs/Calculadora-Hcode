d27717de9fbefe8cff482cd5fefc4c598ec988a7
Wilian <wilian.batista.santos@gmail.com>
2022-09-20 22:32:12 -0300

    Eventos

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,25 +8,60 @@ class CalcController {
         
         this._currentDate 
         this.initialize()
+        this.initButtonsEvents()
         
 
     }
 
     initialize(){
 
+        this.setDisplayDateTime();
+
         setInterval(()=>{
+
+            this.setDisplayDateTime();
+            
+        },1000)
+
+        }
+ setDisplayDateTime(){
+
             this.displayData = this.currentDate.toLocaleDateString(this._locale,{
                 day: "2-digit",
-                month: "long",
+                month: "short",
                 year: "numeric"
             })
             this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+    }
 
-        },1000)
+    addEventListenerAll(element, events, fn){
 
-    
+        events.split(' ').forEach(event=>{
+            element.addEventListener(event, fn, false)
+        })
 
     }
+
+    initButtonsEvents(){
+
+       let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+       buttons.forEach((btn, index)=>{
+        
+        this.addEventListenerAll(btn, "click drag", e =>{
+
+            console.log(btn.className.baseVal.replace("btn-", ""))
+        })
+
+        this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+
+            btn.style.cursor = " pointer"
+        })
+
+       })
+
+    }
+
     get displayTime(){
         return this._timeEl.innerHTML
 
